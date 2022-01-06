48b97216321e4d72d72038961c222d07b730404c
gitaepark <parkkitae7@gmail.com>
2022-01-06 10:52:28 +0900

    Create User Model&Auth API

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
+const authRoute = require('./routes/auth')
+
 const app = express()
 
 dotenv.config()
@@ -17,6 +19,8 @@ mongoose
   })
 
 app.use(express.json())
+
+app.use('/api/auth', authRoute)
 
 app.listen(process.env.PORT, () => {
   console.log('Backend server is running')
