2a2c17ed1a347702ae0894731bfc3d69d8f5755b
gitaepark <parkkitae7@gmail.com>
2022-01-13 22:05:33 +0900

    Create User, User List, New User Page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Sidebar from './components/sidebar/Sidebar'
 import Home from './pages/home/Home'
+import NewUser from './pages/newUser/NewUser'
+import User from './pages/user/User'
+import UserList from './pages/userList/UserList'
 
 function App() {
   return (
@@ -10,8 +13,17 @@ function App() {
       <div className="container">
         <Sidebar />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
           </Route>
         </Switch>
       </div>
