eeff2a96749d6ce5e9d2ec286ddae81d3a48b008
gitaepark <parkkitae7@gmail.com>
2022-01-15 20:13:07 +0900

    Create Product, New Product Page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Sidebar from './components/sidebar/Sidebar'
 import Home from './pages/home/Home'
+import NewProduct from './pages/newProduct/NewProduct'
 import NewUser from './pages/newUser/NewUser'
+import Product from './pages/product/Product'
 import ProductList from './pages/productList/ProductList'
 import User from './pages/user/User'
 import UserList from './pages/userList/UserList'
@@ -28,6 +30,12 @@ function App() {
           </Route>
           <Route path="/products">
             <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newProduct">
+            <NewProduct />
           </Route>
         </Switch>
       </div>
